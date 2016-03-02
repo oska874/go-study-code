1f6afcffddad433acc1037e316e3cf4e65048579
ezio <ezio_zhang@outlook.com>
2016-03-03 00:05:15 +0800

    update

--- ctl0.go
+++ ctl0.go
@@ -1,52 +1,66 @@
 package main
 
-import "fmt" 
+import "fmt"
 import "os"
 
 func main() {
 
-    var x int8 = 0
-    if x > 0 {
-        fmt.Printf("0000\n")
-    } else {
-        fmt.Printf("~~~~\n")
-    }
+	var x int8 = 0
+	if x > 0 {
+		fmt.Printf("0000\n")
+	} else {
+		fmt.Printf("~~~~\n")
+	}
 
-    if f,err := os.Open("LICENSE") ; err != nil {
-        fmt.Printf("222\n")
-    } else {
-        fmt.Printf("1111\n")
-        if f != nil {
-            fmt.Printf("3333\n")
-        }
-    }
+	if f, err := os.Open("LICENSE"); err != nil {
+		fmt.Printf("222\n")
+	} else {
+		fmt.Printf("1111\n")
+		if f != nil {
+			fmt.Printf("3333\n")
+		}
+	}
 
-    a := [5]int{1,2,3,4,5}
+	a := [5]int{1, 2, 3, 4, 5}
 
-    for i := 0;i<5;i++ {
-        fmt.Printf("%d ",a[i])
-    }
-    fmt.Printf("\n")
+	for i := 0; i < 5; i++ {
+		fmt.Printf("%d ", a[i])
+	}
+	fmt.Printf("\n")
 
-    for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
-        a[i], a[j] = a[j], a[i]
-        fmt.Printf("%d\n",i)
-        if  i ==  1 {
-            fmt.Printf("9999")
-            continue
-            fmt.Printf("8888")
-        }
-    }
+	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+		fmt.Printf("%d\n", i)
+		if i == 1 {
+			fmt.Printf("9999")
+			continue
+			fmt.Printf("8888")
+		}
+	}
 
-    for i := 0;i<5;i++ {
-        fmt.Printf("%d ",a[i])
-    }
-    fmt.Printf("\n")
+	for i := 0; i < 5; i++ {
+		fmt.Printf("%d ", a[i])
+	}
+	fmt.Printf("\n")
 
+	i := 0
+	for i < 10 {
+		i++
+		fmt.Printf("aa %d \n", i)
+	}
 
-    i := 0
-    for i < 10{
-        i ++
-        fmt.Printf("aa %d \n",i)
-    }
+	switch {
+	case 1 > 2:
+	case 0 < 1:
+		fmt.Printf("aaa\n")
+	case 1 < 2:
+	}
+
+	i = 0
+	switch i {
+	case 0:
+		fallthrough
+	case 1:
+		print("BBB ", i, "\n")
+	}
 }
--- type1.go
+++ type1.go
@@ -5,19 +5,52 @@ import "fmt"
 func main() {
 
 	var str string = "hello world"
-	fmt.Printf(str+"\n")
+	fmt.Printf(str + "\n")
 
 	s := []rune(str)
 	s[0] = '1'
 	s2 := string(s)
-	fmt.Printf(s2+"\n")
+	fmt.Printf(s2 + "\n")
 
-	var c complex64 = 5+5i
+	var c complex64 = 5 + 5i
 	fmt.Printf("Value is: %v\n", c)
 
 	var a int32 = 0x12345678
 	var a2 int64 = 0x12345678abcd1234
 	var a3 uint16 = 0xf1f2
 
-	fmt.Printf("%x %x %x\n",a,a2,a3)
+	fmt.Printf("%x %x %x\n", a, a2, a3)
+
+	var a4 [3]int32
+	a4[0] = 0
+	a4[1] = 1
+	a4[2] = 2
+	print(a4[2], "\n")
+
+	a5 := [...]int8{1, 2, 3, 4, 5}
+	print(a5[2], "\n")
+
+	sl := make([]int, 10)
+
+	sl[9] = 9
+	sl = append(sl, 10)
+
+	fmt.Printf("%d\n", sl[10])
+
+	mon := map[string]int{"mon": 1}
+	fmt.Printf("%d \n", mon["mon"])
+	mon2 := make(map[string]int)
+	mon2["abc"] = 23
+	fmt.Printf("%d \n", mon2["abc"])
+	mon2["abc12"] = 2333
+	fmt.Printf("%d \n", mon2["abc12"])
+	delete(mon2, "abc")
+	fmt.Printf("%d \n", mon2["abc"])
+	v, ok := mon2["abc"]
+	fmt.Printf("%d ", v, ok)
+
+	strs := [...]string{"asSASA", "ddd", "dsjkdsjs", "asSASA", "ddd", "dsjkdsjs", "dk"}
+	for k, str1 := range strs {
+		fmt.Printf("%d %s len %d\n", k, str1, len(str1))
+	}
 }
