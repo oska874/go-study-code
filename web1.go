12861856429cb2f716051eeae2c2964496d17cd7
ezio <ezio_zhang@outlook.com>
2016-03-13 23:18:56 +0800

    update

--- web1.go
+++ web1.go
@@ -14,6 +14,7 @@ func rootHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Length", fmt.Sprint(len(RESPONSE_TEMPLATE)))
 	w.Write([]byte(RESPONSE_TEMPLATE))
 }
+
 func main() {
 	http.HandleFunc(fmt.Sprintf("%s:%d/", SERVER_DOMAIN, SERVER_PORT), rootHandler)
 	http.ListenAndServeTLS(fmt.Sprintf(":%d", SERVER_PORT), "rui.crt", "rui.key", nil)
